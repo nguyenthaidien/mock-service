07357e211cd69ac8e9c3314fcbbaa9ca21e8130e
nguyenthaidien <nguyenthaidien@gmail.com>
2025-07-15 14:54:07 +0700

    Xac nhan

--- src/main/java/com/example/mock_service/MockServiceApplication.java
+++ src/main/java/com/example/mock_service/MockServiceApplication.java
@@ -2,13 +2,8 @@ package com.example.mock_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
 public class MockServiceApplication {
 
 	public static void main(String[] args) {
