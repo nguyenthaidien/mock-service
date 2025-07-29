f48da411a0c7d0f2dad8e9e2f1eabb70e00351a7
nguyenthaidien <nguyenthaidien@gmail.com>
2025-07-29 16:48:40 +0700

    Cap nhat code

--- src/main/java/com/example/mock_service/MockController.java
+++ src/main/java/com/example/mock_service/MockController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/mocks")
+@RequestMapping
 public class MockController {
 
-    @GetMapping("/hello")
+    @GetMapping("/mock/hello")
     public String sayHello() {
         return "Hello, World!";
     }
